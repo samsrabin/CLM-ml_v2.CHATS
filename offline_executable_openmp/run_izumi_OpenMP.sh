8bb0602c5c795cf8c97b7ae7a617226416b03482
Sam Rabin <sam.rabin@gmail.com>
2026-07-06 13:40:48 -0600

    Attempting to get izumi Intel running

--- offline_executable_openmp/run_izumi_OpenMP.sh
+++ offline_executable_openmp/run_izumi_OpenMP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N CLMml_CHATS7_log_openMP_OneThread
 #PBS -A CESM0029
-#PBS -q develop
+#PBS -q medium
 #PBS -l select=1:ncpus=1:mem=32GB
 #PBS -l walltime=00:30:00
 #PBS -j oe
@@ -10,35 +10,24 @@
 set -euo pipefail
 
 module --force purge
-module load ncarenv/24.12
-module load cesmdev/1.0
-module load conda/latest
-module load nco
-module load craype
-module load cmake
-#module load gcc/13.2.0
-module load intel/2025.1.0
-module load mkl/2025.1.0
-module load ncarcompilers/1.0.0
-#module load cuda/12.3.2
-module load libfabric/2.1.0
-module load openmpi/5.0.7
-module load hdf5-mpi/1.12.3
-module load netcdf-mpi/4.9.3
-module load parallel-netcdf/1.14.0
 
-#
-export LIB_NETCDF=$NCAR_LDFLAGS_NETCDF; export MOD_NETCDF=$NCAR_INC_NETCDF
-cd /glade/u/home/lchou/CLM-ml_Sam/offline_executable_openmp
+module load compiler/intel/20.0.1
+module load mpi/2.3.3/intel/20.0.1
+module load openmpi/4.0.3/intel/20.0.1
+module load tool/hdf5/1.12.0/intel/20.0.1
+module load tool/netcdf/4.7.4/intel/20.0.1
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
 
 HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
 DATETIME=$(date +"%Y%m%d_%H%M%S")
 LOGFILE="run.${HASH}.${DATETIME}_OpenMP_OneThread.log"
 
 echo "Cleaning previous builds..."
-make clean
+make -f Makefile_izumi clean
 echo "Building the executable..."
-make 
+make -f Makefile_izumi
 echo "Running the executable with OpenMP..."
 
 export OMP_STACKSIZE=8G
